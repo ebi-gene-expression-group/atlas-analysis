c599c77917b57e84cfa6886a34bc7f1ae0cb41fa
Maria Keays <mkeays@ebi.ac.uk>
2013-09-19 16:11:51 +0100

    microarray quality assessment scripts

---
+++ analysis/qc/arrayQC.R
@@ -0,0 +1,190 @@
+#!/usr/bin/env Rscript
+#
+# Run quality assessment on microarray data prior to loading to Atlas, using
+# the arrayQualityMetrics Bioconductor package. This script requires a
+# tab-delimited annotation file mapping factor values to raw data filenames,
+# created by arrayQC.pl.
+
+# atlasArrayQC
+# 	- Run arrayQualityMetrics for an experiment-array design.
+# Arguments:
+# 	- annotationFile : filename of annotation file containing mapping between
+# 	factor values and raw data files.
+# 	- exptType : affy, agil1 or agil2
+# 	- exptAcc : experiment accession
+# 	- arrayDesign : array design accession
+# 	- outDir : directory to write quality report into
+# 	- miRBaseFile : path to file with miRBase probe name mappings (0 for a
+# 	non-miRNA experiment)
+atlasArrayQC <- function(annotationFile, exptType, exptAcc, arrayDesign, outDir, miRBaseFile) {
+	# Make a title for the report
+	reportTitle <- paste("Microarray quality metrics report for", exptAcc, "on array design", arrayDesign)
+	
+	# The name of the column with factor values in the annotations file. This
+	# will be "FactorValue" for one-colour experiments but will change to "Cy5"
+	# for two-colour ones.  The values in this column are used to colour points
+	# in the plots in the report.
+	factorColName <- "FactorValue"
+
+	# Read in raw data and add factor values to object created.
+	# Affymetrix data
+	if(exptType == "affy") {
+		# Read in the annotation file with factor values and corresponding raw data
+		# files.
+		annotations <- read.delim(annotationFile, header = TRUE, stringsAsFactors = FALSE)
+		rownames(annotations) <- annotations$AssayName
+
+		# Create Biobase AnnotatedDataFrame object with annotations.
+		library(Biobase)
+		annotDF <- new("AnnotatedDataFrame", annotations)
+	
+		# Load oligo package.
+		library(oligo)
+		
+		# Read files into ExpressionFeatureSet object.
+		dataSet <- try({read.celfiles(annotations$FileName)})
+		if(class(dataSet) == "try-error") {
+			return(dataSet)
+		}
+		
+		# Add annotations to the object.
+		phenoData(dataSet) <- annotDF
+	}
+	# Agilent data.
+	# ***For miRNA do we need to subset probes prior to QM calc??
+	# One-colour Agilent data
+	else if(exptType == "agil1") {
+		# Read in the annotation file with factor values and corresponding raw data
+		# files.
+		annotations <- read.delim(annotationFile, header = TRUE, stringsAsFactors = FALSE)
+		rownames(annotations) <- annotations$AssayName
+
+		# Create Biobase AnnotatedDataFrame object with annotations.
+		library(Biobase)
+		annotDF <- new("AnnotatedDataFrame", annotations)
+	
+		# Load limma package
+		library(limma)
+		
+		# Read in the data to an "EListRaw" object.
+		dataSet <- try({read.maimages(annotations$FileName, source="agilent", green.only=TRUE)})
+		
+		# If we have a miRBase mapping file, subset the data to only include
+		# probes that are there.
+		if(miRBaseFile != 0) {
+			dataSet <- subsetProbes(dataSet, miRBaseFile)
+		}
+	
+		# Make it into an "NChannelSet" object for arrayQualityMetrics to read.
+		# For this need to put the expressions into an AssayData object.
+		aData <- assayDataNew(storage.mode = "lockedEnvironment", exprs = dataSet$E)
+		# Add the rownames from the annotations data frame (filenames) as the sample names.
+		sampleNames(aData) <- rownames(annotations)
+		# Create the NChannelSet object with the expressions and annotations.
+		dataSet <- new("NChannelSet", assayData = aData, phenoData = annotDF)
+		
+		# Check things worked and return the error if not.
+		if(class(dataSet) == "try-error") {
+			return(dataSet)
+		}
+	}
+	# Two-colour Agilent data
+	else if(exptType == "agil2") {
+		# Use the "Cy5" column from the annotations to colour points in the
+		# report.
+		factorColName <- "Cy5"
+
+		# Load limma
+		library(limma)
+
+		# Create a "targets" data frame to show which samples are labelled with
+		# which dye in the report.
+		targets <- readTargets(annotationFile)
+		
+		# Read in the data to an "RGList" object.
+		dataSet <- try({read.maimages(targets, source="agilent")})
+		
+		# Check things worked and return the error if not.
+		if(class(dataSet) == "try-error") {
+			return(dataSet)
+		}
+		
+		# If we have a miRBase mapping file, subset the data to only include
+		# probes that are there.
+		if(miRBaseFile != 0) {
+			dataSet <- subsetProbes(dataSet, miRBaseFile)
+		}
+	}
+	# Something we don't recognise.
+	else { stop(paste("Can't handle", exptType, "experiments yet", sep=" ")) }
+
+	# Load arrayQualityMetrics package
+	library(arrayQualityMetrics)
+	
+	# Run quality metrics calculation
+	arrayQualityMetrics(expressionset = dataSet, 
+						outdir = outDir,
+						force = TRUE,
+						do.logtransform = TRUE,
+						intgroup = factorColName,
+						spatial = FALSE,
+						reporttitle = reportTitle)
+	
+}
+
+
+# Copied this from normalizeOneExperiment.R but should prob take it out to a
+# separate script to source.
+# subsetProbes
+#  - For microRNA, we only want to normalize using probesets that are mapped to
+#  the latest release of miRBase (www.mirbase.org). Subset the data here.
+# ARGUMENTS:
+# 	- dataSet <- for now either EListRaw or RGList object.
+# 	- miRBaseFile <- filename for miRBase mappings.
+subsetProbes <<- function(dataSet, miRBaseFile) {
+
+	print(paste("There are", nrow(dataSet), "rows of data"))
+	
+	print(paste("Subsetting data for probes found in", miRBaseFile))
+
+	# Read file with miRBase mappings into a data frame.
+	miRBaseProbeMapping <- read.delim(miRBaseFile, stringsAsFactors = FALSE)
+	
+	# Probesets can be repeated; take unique set from design_element column in
+	# miRBase mapping data frame.
+	design_element <- unique(miRBaseProbeMapping$design_element)
+
+	# Subset the data, taking only probesets from miRBase mapping file.
+	# Agilent data is read in to one of two classes:
+	# 	- "EListRaw" : 1-colour
+	# 	- "RGList" : 2-colour
+	if(class(dataSet) %in% c("EListRaw", "RGList")) { 
+		
+		# Subset dataSet using design_element column from miRBase mapping file.
+		dataSet <- dataSet[which(dataSet$genes$ProbeName %in% design_element), ]
+		
+		# Check if we have any rows left after subsetting.
+		if(nrow(dataSet) == 0) {
+			# If not, that means no probe names matched any of the values in
+			# the design_element column, so die.
+			stop("No probes in raw data match values in design_element column of miRBase mapping file. Cannot proceed.")
+		}
+	}
+	# If it's not an "ElistRaw" or "RGList" object we can't handle it (yet).
+	else { print("Don't know how to subset for this type of array") }
+
+	print(paste("After subsetting there are", nrow(dataSet), "rows remaining"))
+
+	# TODO: add something here to subset for Affymetrix arrays.
+	# Need to think about how to map probes to probesets because before
+	# normalization probeset info is not available?
+	
+	return(dataSet)
+}
+
+
+# Run with arguments if there are any, otherwise don't do anything.
+args <- commandArgs(TRUE)
+if(length(args) > 0) {
+	do.call(atlasArrayQC, as.list(args))
+}
---
+++ analysis/qc/arrayQC.pl
@@ -0,0 +1,234 @@
+#!/usr/bin/env perl
+#
+# Script for running microarray quality assessment. This script will read
+# MAGE-TAB for an experiment and work out which data files belong to which
+# array design, and are associated with which factor value(s). This info is
+# written to a temp file which is read by an R script to do the quality
+# assessment via the arrayQualityMetrics Bioconductor package.
+
+use strict;
+use warnings;
+
+# MAGE-TAB parsing.
+use Magetab4Atlas;
+
+my $exptAccession = shift;
+
+my $qcRscript = "arrayQC.R";
+
+# Path to directory with ArrayExpress/Atlas load directories underneath.
+my $exptsLoadStem = "/net/isilon5/ma/home/arrayexpress/ae2_production/data/EXPERIMENT";
+
+# miRBase mapped array designs -- we need to subset probes if we find one of these.
+my $miRBaseFiles = `ls /nfs/ma/home/atlas3-production/bioentity_properties/mirbase/*.A-*.tsv`;
+# Put them in an array.
+my @A_miRBaseFiles = split "\n", $miRBaseFiles;
+
+# Get the pipeline (e.g. MEXP, MTAB, GEOD, ...) for this experiment.
+(my $pipeline = $exptAccession) =~ s/E-(\w{4})-\d+/$1/;
+
+# Experiment load directory and IDF filename.
+my $loadDir = "$exptsLoadStem/$pipeline/$exptAccession";
+my $idfFilename = "$loadDir/$exptAccession.idf.txt";
+
+# Read the MAGE-TAB.
+my $magetab4atlas = Magetab4Atlas->new( "idf_filename" => $idfFilename );
+
+# Next need to sort the raw data files by array design and within that
+# by factor value. Use a hash like:
+# 	$H->{ <array design 1> }->{ <factor value(s) 1> } = [ <file 1>, <file 2>, <file 3> ]
+# 	                        ->{ <factor value(s) 2> } = [ <file 4>, <file 5>, <file 6> ]
+# 	  ->{ <array design 2> }->{ <factor value(s) 3> } = [ <file 7>, <file 8>, <file 9> ]
+# 	  ...
+my ($H_arraysToFactorValuesToFiles, $experimentType) = &makeArraysToFactorValuesToFiles($magetab4atlas, $loadDir);
+
+# Write the factor value and filename information into a temporary text file
+# for each array design. This will be read by R and then deleted.
+foreach my $arrayDesign (keys %{ $H_arraysToFactorValuesToFiles }) {
+	# Check if the array design has a miRBase mapping file
+	# Flag
+	my $miRBaseFile = 0;
+	# Go through the files and look for a match.
+	# This bit could end up taking a while if there are a lot of files?
+	foreach my $file (@A_miRBaseFiles) {
+		# If the array design matches log and remember the file.
+		if($file =~ /$arrayDesign/) {
+			print "miRNA array detected\n";
+			$miRBaseFile = $file;
+		}
+	}
+	
+	# Name for temp file to write annotations to.
+	my $tempFile = ".$exptAccession"."_$arrayDesign.tsv";
+	# Create annotations temp file.
+	open(my $tmpFH, '>', $tempFile) or die "Can't create file \"$tempFile\": $!\n";
+	
+	# Write factor values and corresponding raw data filename.
+	# If this is two-colour data we want to include the label info in the file.
+	# Create a file with headings like:
+	# 	AssayName	Cy3	Cy5	FileName
+	if($experimentType eq "agil2") {
+		# Ref to empty hash to remember two-colour annotations and filenames.
+		my $H_twoColourAnnotations = {};
+		# Go through the factor values for this array design...
+		foreach my $factorValue (keys %{ $H_arraysToFactorValuesToFiles->{ $arrayDesign } }) {
+			# Go through the assays for this factor value...
+			foreach my $assayName (keys %{ $H_arraysToFactorValuesToFiles->{ $arrayDesign }->{ $factorValue } }) {
+				# Get the label (Cy3 or Cy5)
+				(my $label = $assayName) =~ s/.*\.(Cy\d)$/$1/;
+				# Make a version of the assay name without the label.
+				(my $assayNameNoLabel = $assayName) =~ s/\.Cy\d$//;
+				
+				# Add the factor value to the hash for this assay for this label.
+				$H_twoColourAnnotations->{ $assayNameNoLabel }->{ $label } = $factorValue;
+				# Add the file name for this assay as well.
+				$H_twoColourAnnotations->{ $assayNameNoLabel }->{ "filename" } = $H_arraysToFactorValuesToFiles->{ $arrayDesign }->{ $factorValue }->{ $assayName };
+			}
+		}
+
+		# Write header.
+		print $tmpFH "AssayName\tCy3\tCy5\tFileName";
+		# Write the annotations.
+		foreach my $assayNameNoLabel (keys %{ $H_twoColourAnnotations }) {
+			print $tmpFH "\n$assayNameNoLabel\t";
+			print $tmpFH $H_twoColourAnnotations->{ $assayNameNoLabel }->{ "Cy3" }, "\t";
+			print $tmpFH $H_twoColourAnnotations->{ $assayNameNoLabel }->{ "Cy5" }, "\t";
+			print $tmpFH $H_twoColourAnnotations->{ $assayNameNoLabel }->{ "filename" };
+		}
+	}
+	# For 1-colour arrays, don't need the Cy3/Cy5 info.
+	else {
+		# Write header.
+		print $tmpFH "AssayName\tFactorValue\tFileName";
+		# Go through the factor values for this array...
+		foreach my $factorValue (keys %{ $H_arraysToFactorValuesToFiles->{ $arrayDesign } }) {
+			# Go through the assays for this factor value...
+			foreach my $assayName (keys %{ $H_arraysToFactorValuesToFiles->{ $arrayDesign }->{ $factorValue } }) {
+				# Write annotations.
+				print $tmpFH "\n$assayName\t$factorValue\t".$H_arraysToFactorValuesToFiles->{ $arrayDesign }->{ $factorValue }->{ $assayName };
+			}
+		}
+	}
+	close $tmpFH;
+
+	# Create directory name for this experiment/array design.
+	my $reportDir = $exptAccession."_$arrayDesign"."_QM";
+	# Run R script.
+	my $qcRscriptOutput = `$qcRscript $tempFile $experimentType $exptAccession $arrayDesign $reportDir $miRBaseFile 2>&1`;
+
+	# Check for errors in the R output.
+	if($qcRscriptOutput =~ /error/i) {
+		# Warn that QC had problems but continue with the next array design (if any).
+		print "[WARNING] Error during quality metrics calculation for array $arrayDesign, outout from R below.\n------------\n$qcRscriptOutput\n------------\n";
+	}
+	
+	# Delete the no longer needed temp file.
+	`rm $tempFile`;
+
+	# The HTML report contains the full path to the raw data files in th load
+	# directory. This looks annoying and is not necessary, so remove it. The
+	# path is in index.html and arrayQualityMetrics.js.
+	&removeLoadDirFromReport($reportDir, $loadDir);
+}
+
+
+# Subroutines
+
+# &makeArraysToFactoValuesToFiles
+#  - Creates a hash sorting out the data files by array design and then factor value.
+#  - E.g.:
+# 	$H->{ <array design 1> }->{ <factor value(s) 1> }->{ <assay name 1> } = <file 1>
+# 	                        ->{ <factor value(s) 2> }->{ <assay name 2> } = <file 2>
+# 	  ->{ <array design 2> }->{ <factor value(s) 3> }->{ <assay name 3> } = <file 3>
+# 	  ...
+# Arguments:
+# 	- $magetab4atlas : a Magetab4Atlas object
+# 	- $loadDir : path to load directory containing raw data files.
+sub makeArraysToFactorValuesToFiles {
+	# Magetab4Atlas object and path to load directory.
+	my ($magetab4atlas, $loadDir) = @_;
+	
+	# Experiment type from Magetab4Atlas will be either "one-colour array" or
+	# "two-colour array". Die if it's something else.
+	my $experimentType = $magetab4atlas->get_experiment_type;
+	unless($experimentType eq "one-colour array" || $experimentType eq "two-colour array") {
+		die "This doesn't look like a microarray experiment. Experiment type found is: $experimentType\n";
+	}
+	
+	# Ref to empty hash to fill.
+	my $H_arraysToFactorValuesToFiles = {};
+	# Go through the assays...
+	foreach my $assay4atlas (@{ $magetab4atlas->get_assays }) {
+		# Get the assay name
+		my $assayName = $assay4atlas->get_name;
+		# Get the array design
+		my $arrayDesign = $assay4atlas->get_array_design;
+		# Get the factor(s) and value(s)
+		my $H_factors = $assay4atlas->get_factors;
+		# Get the raw data filename
+		my $arrayDataFile = $loadDir."/".$assay4atlas->get_array_data_file;
+
+		# For 1-colour array data, need to tell the R script whether this is
+		# Affymetrix or Agilent (or other -- for now we only handle Affy and Agil
+		# data). Work this out based on the file extension for now -- another
+		# possibility might be to parse the ADF, but there is no standard way
+		# to record manufacturer there so that might not be ideal.
+		if($experimentType eq "one-colour array") {
+			if($arrayDataFile =~ /\.cel$/i) { $experimentType = "affy"; }
+			else { $experimentType = "agil1"; }
+		} elsif($experimentType eq "two-colour array") {
+			$experimentType = "agil2";
+		}
+
+		# Push all factor values onto an array.
+		my @A_factorValues = ();
+		foreach my $factor (keys %{ $H_factors }) {
+			push @A_factorValues, $H_factors->{ $factor };
+		}
+
+		# Stick the factor values together if there's more than one. If there's
+		# only one this just returns the factor value by itself.
+		my $factorValue = join ", ", @A_factorValues;
+		
+		$H_arraysToFactorValuesToFiles->{ $arrayDesign }->{ $factorValue }->{ $assayName } = $arrayDataFile;
+	}
+	return ($H_arraysToFactorValuesToFiles, $experimentType);
+}
+
+
+# &removeLoadDirFromReport
+# 	- Remove the full path to the load directory from index.html and arrayQualityMetrics.js.
+# Arguments:
+# 	- $reportDir : the directory containing the output arrayQualityMetrics.
+# 	- $loadDir : the load directory path to remove.
+sub removeLoadDirFromReport {
+	my ($reportDir, $loadDir) = @_;
+	
+	# We need to fix the HTML report and the JavaScript file.
+	my @A_filesToFix = ("$reportDir/index.html", "$reportDir/arrayQualityMetrics.js");
+
+	foreach my $original (@A_filesToFix) {
+		# A filename for a temp file to write to.
+		my $temp = "$reportDir/.temp";
+		
+		# Open the report for reading.
+		open(my $originalFH, "<", $original) or die "Can't open $original : $!\n";
+		# Open the temp file for writing.
+		open(my $tempFH, ">", $temp) or die "Can't open $temp : $!\n";
+		
+		# Go through the report line by line...
+		while(defined(my $line = <$originalFH>)) {
+			# Remove the load directory path if it's there.
+			$line =~ s/$loadDir\/*//g;
+			# Write the line to the temp file.
+			print $tempFH $line;
+		}
+		# Close them.
+		close($originalFH);
+		close($tempFH);
+
+		# Overwrite the original report with the fixed one.
+		`mv $temp $original`;
+	}
+}
+
