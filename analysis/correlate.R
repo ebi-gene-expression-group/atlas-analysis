a275fe793dba6ff0830370ab4d6235772429ddd1
Maria Keays <mkeays@ebi.ac.uk>
2014-01-02 17:13:18 +0000

    correlate.R allows different column headings for each file

--- analysis/correlate.R
+++ analysis/correlate.R
@@ -1,51 +1,133 @@
 #!/usr/bin/env Rscript
 # 
-# Calculate Pearson's product-moment correlation coefficient using two sets of
-# RNA-seq read counts (or *PKMs) produced by different quantification methods.
-# Produce a scatterplot and display correlation coefficient on it.
+# Calculate Pearson's product-moment correlation coefficient or Spearman Rank
+# correlation using two sets of RNA-seq read counts (or *PKMs) produced by
+# different quantification methods.  Produce a scatterplot and display
+# correlation coefficient on it.
 #
-# Provide two tab-delimited text files with genes in rows and sequencing runs in columns.
-# Provide comma-separated gene IDs and/or run IDs to use for the plot. To use
-# all genes and/or all runs, use "all" instead (see below examples).
+# Provide two tab-delimited text files with genes in rows and sequencing runs
+# (or averaged counts/FPKMs for a group of replicates) in columns.
+# Provide gene IDs in a file and/or column headings to subset from the above
+# files. To use all genes and/or all runs, use "all" instead (see below
+# examples).
 # 
-# For each tab-delimited file, counts(/*PKMs) from all runs specified are
+# For each tab-delimited file, counts(/*PKMs) from all columns specified are
 # summed for each gene. The resulting two vectors of counts are correlated and
 # plotted.
 #
 # Examples:
-# 1. All genes and all runs:	
-# 	Rscript correlate.R all all <counts file 1> <counts file 2> <plot filename>
+# 1. All genes and all columns:	
+# 	Rscript correlate.R all <counts file 1> all <counts file 2> all <plot filename> pearson[|spearman]
 #
-# 2. Only some genes but all runs:
-# 	Rscript correlate.R ENSG0001,ENSG1234,ENSG0021,ENSG9991 all <counts file 1> <counts file 2> <plot filename>
+# 2. Only some genes but all columns:
+# 	Rscript correlate.R <gene IDs file> <counts file 1> all <counts file 2> all <plot filename> pearson[|spearman] 
 #
-# 3. All genes but only some runs:
-# 	Rscript correlate.R all SRR001,SRR003,SRR009 <counts file 1> <counts file 2> <plot filename>
+# 3. All genes but only some columns:
+# 	(a) sequencing runs from the same experiment.
+# 		Rscript correlate.R all <counts file 1> SRR001,SRR002 <counts file 2> SRR001,SRR002 <plot filename> pearson[|spearman]
+# 	(b) averaged biological replicates from different experiments (e.g. heart
+# 	was in column "g8" in one experiment and "g10" in the other)
+# 		Rscript correlate.R all <counts file 1> g8 <counts file 2> g10 <plot filename> pearson[|spearman]
 
 
 # correlate
 # ARGUMENTS:
 # 	- genes : either "all" or name of a file containing a list of gene IDs (one per line) to use in correlation.
-# 	- runs : either "all" or a comma-separated list of run accessions to use in correlation.
 # 	- countsFile1 : name of first file containing data to use in correlation.
+# 	- cols1 : comma-separated list of column headings to select from file 1.
 # 	- countsFile2 : name of second file containing data to use in correlation.
+# 	- cols2 : comma-separated list of column headings to select from file 2.
 # 	- outFile : name of PDF file to write plot of correlation to.
 # 	- corType : either "pearson" for Pearson's product-moment correlation, or "spearman" for Spearman Rank correlation.
-correlate <<- function(genes, runs, countsFile1, countsFile2, outFile, corType) {
+correlate <<- function(genes, countsFile1, cols1, countsFile2, cols2, outFile, corType) {
 	
 	# Get labels for plot axes from filenames.
 	counts1Lab <- basename(countsFile1)
 	counts2Lab <- basename(countsFile2)
 
-	# Read in counts and sort the rows by gene IDs.
-	counts1 <- read.delim(countsFile1, header=TRUE, sep="\t")
+	# Read in counts.
+	counts1 <- read.delim(countsFile1, header=TRUE, stringsAsFactors=FALSE)
 	rownames(counts1) <- counts1[,1]
-	counts1 <- subset(counts1, select = -1, drop=FALSE)
-	counts1 <- counts1[order(rownames(counts1)),,drop=FALSE]
+	counts1[,1] <- NULL
 	
-	counts2 <- read.delim(countsFile2, header=TRUE, sep="\t")
+	counts2 <- read.delim(countsFile2, header=TRUE, stringsAsFactors=FALSE)
 	rownames(counts2) <- counts2[,1]
-	counts2 <- subset(counts2, select = -1, drop=FALSE)
+	counts2[,1] <- NULL
+
+	# Subset selected columns from the first file if need be.
+	if(cols1 != "all") {
+		
+		cols1 <- unlist(strsplit(cols1, ","))
+
+		counts1 <- counts1[,cols1, drop=FALSE]
+	} else {
+		cat(paste("Using all columns from", countsFile1, "\n"))
+	}
+	
+	# Subset selected columns from the second file if need be.
+	if(cols2 != "all") {
+		
+		cols2 <- unlist(strsplit(cols2, ","))
+
+		counts2 <- counts2[,cols2, drop=FALSE]
+	} else {
+		cat(paste("Using all columns from", countsFile2, "\n"))
+	}
+
+	# Read genes from a file unless "all" is passed.
+	if(genes != "all") {
+		
+		cat(paste("Using genes from", genes, "\n"))
+		genes <- scan(file=genes, what="character")
+		
+		counts1 <- counts1[genes, , drop=FALSE]
+
+		# Check for genes that weren't found in the first counts matrix.
+		if(length(grep("NA", rownames(counts1))) > 0 ) {
+			# Get IDs of genes that weren't found.
+			unwantedGenes1 <- genes[grep("NA", rownames(counts1))]
+			
+			# Log that they weren't found and will be removed.
+			cat(paste("The following genes were not found in", countsFile1, "\n"))
+			cat(paste(unwantedGenes1, collapse="\n"))
+			cat("\nThey will be removed.\n\n")
+			
+			# Remove them from counts1.
+			counts1 <- counts1[-grep("NA", rownames(counts1)),,drop=FALSE]
+
+			# Also remove them from counts2 if necessary.
+			if(length(which(rownames(counts2) %in% unwantedGenes1)) > 0) {
+				counts2 <- counts2[-which(rownames(counts2) %in% unwantedGenes1),,drop=FALSE]
+			}
+		}
+		
+		counts2 <- counts2[genes, , drop=FALSE]
+		
+		# Check for genes that weren't found in the second counts matrix.
+		if(length(grep("NA", rownames(counts2))) > 0 ) {
+			# Get IDs of genes that weren't found.
+			unwantedGenes2 <- genes[grep("NA", rownames(counts2))]
+			
+			# Log that they weren't found and will be removed.
+			cat(paste("The following genes were not found in", countsFile2, "\n"))
+			cat(paste(unwantedGenes2, collapse="\n"))
+			cat("\nThey will be removed.\n\n")
+			
+			# Remove them from counts2.
+			counts2 <- counts2[-grep("NA", rownames(counts2)),,drop=FALSE]
+
+			# Also remove them from counts1 if necessary.
+			if(length(which(rownames(counts1) %in% unwantedGenes2)) > 0) {
+				counts1 <- counts1[-which(rownames(counts1) %in% unwantedGenes2),,drop=FALSE]
+			}
+		}
+			
+	} else {
+		cat("\nUsing all genes\n")
+	}
+
+	# Sort rows by gene IDs.
+	counts1 <- counts1[order(rownames(counts1)),,drop=FALSE]
 	counts2 <- counts2[order(rownames(counts2)),,drop=FALSE]
 
 	# Make sure that the row names in each set of counts are identical. It
@@ -53,28 +135,6 @@ correlate <<- function(genes, runs, countsFile1, countsFile2, outFile, corType) 
 	# method with counts from a totally different gene from the other method.
 	stopifnot(all(rownames(counts1) == rownames(counts2)))
 	
-	# Subset selected runs if need be.
-	if(runs != "all") {
-		
-		runs <- unlist(strsplit(runs, ","))
-
-		counts1 <- counts1[,runs, drop=FALSE]
-		counts2 <- counts2[,runs, drop=FALSE]
-	} else {
-		print("Using all sequencing runs.")
-	}
-	# Read genes from a file unless "all" is passed.
-	if(genes != "all") {
-		
-		print(paste("Using genes from", genes))
-		genes <- scan(file=genes, what="character")
-		
-		counts1 <- counts1[genes, , drop=FALSE]
-		counts2 <- counts2[genes, , drop=FALSE]
-	} else {
-		print("Using all genes")
-	}
-
 	# Add counts for each gene and put the two vectors into a data frame.
 	counts1Sum <- apply(counts1, 1, function(x) { sum(x) })
 	counts2Sum <- apply(counts2, 1, function(x) { sum(x) })
@@ -104,7 +164,7 @@ cor.scatterplot <- function(df, colA, colB, counts1Lab, counts2Lab, outFile, cor
 	# add 0.1 to all counts (why??).
 	del <- 0.1
 
-	print(paste("Running correlation with method =", corType))
+	cat(paste("Running correlation with method =", corType, "\n"))
 
 	# use cor() to get Pearson's or Spearman's correlation coefficient, and round it to 2 decimal places.
     x<-round(cor(df[,colA], df[,colB], method=corType), 2)
