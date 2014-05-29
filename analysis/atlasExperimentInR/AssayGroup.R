dd8fa224762b20d6ed0bdef6e2f5029f7cbe1ebc
Maria Keays <mkeays@ebi.ac.uk>
2014-05-29 18:11:32 +0100

    renamed file, added function to parse SDRF

--- analysis/atlasExperimentInR/AssayGroup.R
+++ analysis/atlasExperimentInR/AssayGroup.R
@@ -8,9 +8,8 @@ setClass( "AssayGroup", slots = c( assays = "vector", assay_group_id = "characte
 # assay_group_id getter
 setGeneric( "assay_group_id", function( object ) standardGeneric( "assay_group_id" ) )
 
-# assay_group_id setter
-setGeneric( "assay_group_id<-", function( object, value ) standardGeneric( "assay_group_id<-" ) )
-
+# assays getter
+setGeneric( "assays", function( object ) standardGeneric( "assays" ) )
 
 ####################
 # AssayGroup Methods
@@ -42,14 +41,5 @@ setMethod( "initialize", "AssayGroup", function( .Object, assayGroupNode ) {
 # assay_group_id getter
 setMethod( "assay_group_id", "AssayGroup", function( object ) object@assay_group_id )
 
-# assay_group_id setter
-setReplaceMethod( "assay_group_id",
-	signature(
-		object="AssayGroup",
-		value="character"
-	),
-	function( object, value ) {
-		object@assay_group_id <- value
-		return( object )
-	}
-)
+# assays
+setMethod( "assays", "AssayGroup", function( object ) object@assays )
