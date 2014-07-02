9931667efbedee1d05516be34bc6582c7a503189
Robert Petryszak <rpetry@ebi.ac.uk>
2014-07-02 18:17:27 +0100

    Fixed variable assignment

--- analysis/differential/diffAtlas.sh
+++ analysis/differential/diffAtlas.sh
@@ -1,8 +1,8 @@
 #!/bin/bash    
 
 # A wrapper script to allow for re-computing of differential expression for a microarraye experiment - so that this can be called from decorate_all_experiments.sh
-$expAcc=$1
-$expTargetDir=$2
+expAcc=$1
+expTargetDir=$2
 
 if [ $# -lt 2 ]; then
   echo "Usage: $0 expAcc expPath"
