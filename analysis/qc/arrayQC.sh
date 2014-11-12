1c25745390352c1b17c9aa11ee5978e9d25791de
Robert Petryszak <rpetry@ebi.ac.uk>
2014-11-12 11:06:56 +0000

    Changes necessary to add microarray QC re-generation to decorate_all_experiments.sh

---
+++ analysis/qc/arrayQC.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+source ${ATLAS_PROD}/sw/atlasprod/irap/gxa_preirap.conf
+source ${ATLAS_PROD}/sw/atlasprod/irap/gxa_preirap_routines.sh
+
+if [ $# -lt 1 ]; then
+   echo "Usage: $0 expAcc"
+   echo "e.g. $0 E-MTAB-1066"
+   exit 1
+fi
+
+expAcc=$1
+pushd ${ATLAS_PROD}/analysis/differential/microarray/experiments/$expAcc
+rm -rf qc
+
+${ATLAS_PROD}/sw/atlasprod/analysis/qc/arrayQC.pl $expAcc
+exitCode=$?
+if [ $exitCode -eq 1 ]; then
+    # The QC procedure succeeded but the experiment failed the QC
+    mv ${ATLAS_PROD}/analysis/differential/microarray/experiments/$expAcc ${ATLAS_PROD}/failedQC/microarray/
+    echo "[QC] Quality control for ${expAcc} has failed - see http://www.ebi.ac.uk/~rpetry/atlas3/failedQC/microarray/${expAcc} for more info"
+elif [ $exitCode -ne 0 ]; then
+    # The QC procedure itself failed (e.g. due to lack of memory) - we don't know if the experiment passes or fails the QC
+    # Perl die() returns exit code=255
+    echo "ERROR: Failed to perform QC for ${expAcc} - exit code: $exitCode" >&2
+    exit 1       
+else
+    # Experiment has passed QC check - move quality report dir into qc/
+    mkdir -p qc
+    mv *_QM qc
+fi
+popd
