ba485b54129435cc3af7a703ee4aa3019170692b
Maria Keays <mkeays@ebi.ac.uk>
2015-03-17 10:25:43 +0000

    fixing generateBaselineHeatmap.R to work with single column data

--- analysis/generateBaselineHeatmap.R
+++ analysis/generateBaselineHeatmap.R
@@ -57,7 +57,7 @@ count_data_columns <- function( fpkmsDataFrame, dataFrameType ) {
 
     # Check that there is more than one column of FPKMs. We can't make a heatmap
     # with less than two columns of data.
-    if( ncol( fpkmsDataFrame[ startCol:ncol( fpkmsDataFrame ) ) < 2 ) {
+    if( ncol( fpkmsDataFrame[ startCol:ncol( fpkmsDataFrame ) ] ) < 2 ) {
         # Warn what has happened.
         warning( "Less than two columns of FPKMs found. Cannot continue." )
         
