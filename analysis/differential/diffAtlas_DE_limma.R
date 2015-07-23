eb3aceb319f8607a3ba3c18d4888f1a3c170a9de
Maria Keays <mkeays@ebi.ac.uk>
2015-07-23 18:16:53 +0100

    removed de-bugging message from limam script

--- analysis/differential/diffAtlas_DE_limma.R
+++ analysis/differential/diffAtlas_DE_limma.R
@@ -482,8 +482,6 @@ run_one_colour_analysis <- function( expAcc, allAnalytics, atlasProcessingDirect
 			testAssayGroupID <- test_assay_group_id( expContrast )
 			contrastBatchEffects <- batch_effects( expContrast )
 
-			print( contrastBatchEffects )
-
 			cat( paste( 
 					   "Processing contrast",
 					   contrastID,
