6a58b84818897ea12e06e8368c7c12e3febe9ad4
Maria Keays <mkeays@ebi.ac.uk>
2015-12-02 16:19:58 +0000

    testing first function in ExpressionAtlasData package

--- analysis/atlasExperimentInR/ExpressionAtlasData/R/functions.R
+++ analysis/atlasExperimentInR/ExpressionAtlasData/R/functions.R
@@ -1,4 +1,36 @@
 
 getAtlasExperiment <- function( experimentAccession ) {
+    
+    # Make sure we were given an experiment accession.
+    if( missing( experimentAccession ) ) {
 
+        stop( "Please provide experiment accession." )
+    }
 
+    # Make sure the experiment accession is in the correct format.
+    if( !grepl( "^E-\\w{4}-\\d+$", experimentAccession ) ) {
+        stop( paste( "\"", experimentAccession, "\" does not look like an ArrayExpress experiment accession. Cannot continue.", sep="" ) )
+    }
+
+    urlBase <- "http://www.ebi.ac.uk/gxa/experiments"
+
+    atlasExperimentSummaryFile <- paste( experimentAccession, "-atlasExperimentSummary.Rdata", sep = "" )
+
+    fullUrl <- paste( urlBase, experimentAccession, atlasExperimentSummaryFile, sep = "/" )
+    
+    cat( paste( "Loading Expression Atlas experiment summary from", fullUrl, "...\n" ) )
+
+    result <- try( load( url( fullUrl ) ) )
+    
+    if( class( result ) == "try-error" ) {
+
+        msg <- geterrmessage()
+
+        stop( paste( "Error encountered while trying to download experiment summary:", msg ) )
+    }
+
+    # If we're still here, things must have worked ok.
+    cat( paste( "Successfully loaded experimentSummary object for", experimentAccession, "\n" ) )
+
+    return( experimentSummary )
+}
