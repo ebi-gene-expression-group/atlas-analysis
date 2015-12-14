d07b7e026523efc42ded33bb9889a8a6df01a323
Maria Keays <mkeays@ebi.ac.uk>
2015-12-14 16:08:13 +0000

    fixed typo

--- analysis/atlasExperimentInR/ExpressionAtlasInternal/R/functions.R
+++ analysis/atlasExperimentInR/ExpressionAtlasInternal/R/functions.R
@@ -582,7 +582,7 @@ check_file_exists <- function( filename ) {
 	analysisMethodsList <- .readSeqAnalysisMethods( analysisMethodsFile )
     
     # Create dummy row ranges (copied from SummarizedExperiment class code)
-    partitioning <- PartitioningByEnd( integer( nrow( expressionsMatrix ) ), names = rownames( expressionMatrix ) )
+    partitioning <- PartitioningByEnd( integer( nrow( expressionsMatrix ) ), names = rownames( expressionsMatrix ) )
     dummyRanges <- relist( GRanges(), partitioning )
 
 	# Create SummarizedExperiment
