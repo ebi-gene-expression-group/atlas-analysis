2d8b4cbc09af404f92df919afdec60e35f79edf4
Maria Keays <mkeays@ebi.ac.uk>
2015-12-15 14:12:43 +0000

    added test, NEWS file, moved accession validation to function

--- analysis/atlasExperimentInR/ExpressionAtlas/R/functions.R
+++ analysis/atlasExperimentInR/ExpressionAtlas/R/functions.R
@@ -3,24 +3,34 @@
 #   Expression Atlas experiment.
 getAtlasExperiment <- function( experimentAccession ) {
     
-    # Make sure we were given an experiment accession.
-    if( missing( experimentAccession ) ) {
-
-        stop( "Please provide experiment accession." )
-    }
-
     # Make sure the experiment accession is in the correct format.
-    if( !grepl( "^E-\\w{4}-\\d+$", experimentAccession ) ) {
-        stop( paste( "\"", experimentAccession, "\" does not look like an ArrayExpress experiment accession. Cannot continue.", sep="" ) )
+    if( ! .isValidExperimentAccession( experimentAccession ) ) {
+
+        stop( "Experiment accession not valid. Cannot continue." )
     }
 
     urlBase <- "http://www.ebi.ac.uk/gxa/experiments"
 
-    atlasExperimentSummaryFile <- paste( experimentAccession, "-atlasExperimentSummary.Rdata", sep = "" )
+    atlasExperimentSummaryFile <- paste( 
+        experimentAccession,
+        "-atlasExperimentSummary.Rdata", 
+        sep = "" 
+    )
 
-    fullUrl <- paste( urlBase, experimentAccession, atlasExperimentSummaryFile, sep = "/" )
+    fullUrl <- paste( 
+        urlBase, 
+        experimentAccession, 
+        atlasExperimentSummaryFile, 
+        sep = "/" 
+    )
     
-    cat( paste( "Downloading Expression Atlas experiment summary from", fullUrl, "...\n" ) )
+    cat( 
+        paste( 
+            "Downloading Expression Atlas experiment summary from", 
+            fullUrl, 
+            "...\n" 
+        ) 
+    )
 
     loadResult <- try( load( url( fullUrl ) ) )
     
@@ -28,7 +38,12 @@ getAtlasExperiment <- function( experimentAccession ) {
 
         msg <- geterrmessage()
 
-        stop( paste( "Error encountered while trying to download experiment summary:", msg ) )
+        stop( 
+            paste( 
+                "Error encountered while trying to download experiment summary:", 
+                msg 
+            ) 
+        )
     }
     
     # Make sure experiment summary object exists before trying to return it.
@@ -36,11 +51,19 @@ getAtlasExperiment <- function( experimentAccession ) {
 
     if( class( getResult ) == "try-error" ) {
         
-        stop( "ERROR - Download appeared successful but no experiment summary object was found." )
+        stop( 
+            "ERROR - Download appeared successful but no experiment summary object was found." 
+        )
     }
 
     # If we're still here, things must have worked ok.
-    cat( paste( "Successfully downloaded experiment summary object for", experimentAccession, "\n" ) )
+    cat( 
+        paste( 
+            "Successfully downloaded experiment summary object for", 
+            experimentAccession, 
+            "\n" 
+        ) 
+    )
 
     return( get( "experimentSummary" ) )
 }
@@ -56,35 +79,71 @@ getAtlasData <- function( experimentAccessions ) {
         stop( "Please provide a vector of experiment accessions to download." )
     }
 
+    # Make sure experimentAccessions is a vector.
+    if( ! is.vector( experimentAccessions ) ) {
+        
+        stop( "Please provide experiment accessions as a vector." )
+    }
+
     # Go through each one and download it, creating a list.
     # So that the list has the experiment accessions as the names, use them as
     # names for the vector before starting to create the list.
     names( experimentAccessions ) <- experimentAccessions
 
     experimentSummaryList <- lapply( experimentAccessions, function( experimentAccession ) {
-        getAtlasExperiment( experimentAccession )
+
+        if( .isValidExperimentAccession( experimentAccession ) ) {
+            
+            getAtlasExperiment( experimentAccession )
+
+        } else {
+
+            warning( 
+                paste( 
+                    "Not attempting to download data for invalid accession \"", 
+                    experimentAccession, 
+                    "\"", 
+                    sep="" 
+                ) 
+            )
+        }
     } )
+
+    # Make sure we got something back -- if all the accessions were invalid
+    # then experimentSummaryList may be empty.
 
     return( experimentSummaryList )
 }
 
 
+# .isValidExperimentAccession
+#   - Return TRUE if experiment accession matches expected ArrayExpress
+#   experiment accession pattern. Return FALSE otherwise.
+.isValidExperimentAccession <- function( experimentAccession ) {
 
+    if( missing( experimentAccession ) ) {
+        
+        warning( "Accession missing. Cannot validate." )
 
+        return( FALSE )
+    }
 
+    if( !grepl( "^E-\\w{4}-\\d+$", experimentAccession ) ) {
+        
+        warning( 
+            paste( 
+                "\"", 
+                experimentAccession, 
+                "\" does not look like an ArrayExpress experiment accession. Please check.", 
+                sep="" 
+            ) 
+        )
+        
+        return( FALSE )
 
+    } else {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+        return( TRUE )
+    }
+}
 
