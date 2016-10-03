d09c2c52a997b007495b4cbdfbd77508ae585dd1
Maria Keays <mkeays@ebi.ac.uk>
2016-10-03 09:33:03 +0100

    updating to RH7

--- analysis/gsa/api/gsaRestApiServer.sh
+++ analysis/gsa/api/gsaRestApiServer.sh
@@ -4,7 +4,7 @@
 ## source /homes/oracle/ora11setup.sh
 ## export LD_LIBRARY_PATH=/nfs/production3/ma/home/rpetry/workspace/instantclient:$LD_LIBRARY_PATH
 export ATLAS_PROD=$ATLAS_PROD
-export PATH=${ATLAS_PROD}/sw/nodejs/node-v0.12.7-linux-x64/bin:$ATLAS_PROD/sw/atlasinstall_prod/R_install/bin/:$PATH
+export PATH=${ATLAS_PROD}/sw/nodejs/node-v0.12.7-linux-x64/bin:$ATLAS_PROD/sw/atlasinstall_prod/R_install_3.3.0_rh7/bin/:$PATH
 
 cp ${ATLAS_PROD}/sw/atlasinstall_prod/atlasprod/analysis/gsa/api/gsaRestApiServer.js /nfs/production3/ma/home/rpetry/workspace/nodejs/
 # gsaRestApiServer.js needs to be run from the same directory in which the modules it requires are installed, c.f. /nfs/production3/ma/home/rpetry/workspace/nodejs/node_modules
