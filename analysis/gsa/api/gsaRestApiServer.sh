6f74da5dc69285bcf5afb3b3f6536bf8c24166f0
Robert Petryszak <rpetry@ebi.ac.uk>
2017-02-06 17:31:28 +0000

    Since plantain is rh6, adjusted R path accordingly

--- analysis/gsa/api/gsaRestApiServer.sh
+++ analysis/gsa/api/gsaRestApiServer.sh
@@ -2,7 +2,8 @@
 
 # A script to run gsaRestApiServer.js
 export ATLAS_PROD=$ATLAS_PROD
-export PATH=${ATLAS_PROD}/sw/nodejs/node-v0.12.7-linux-x64/bin:$ATLAS_PROD/sw/atlasinstall_prod/R_install_3.3.0_rh7/bin/:$PATH
+# Note that this script currently runs on plantain (rh6), hence no mention of R_install_3.3.0_rh7 in the path below. When the script moves to a RH7 VM, the R path will need adjusting accordingly
+export PATH=${ATLAS_PROD}/sw/nodejs/node-v0.12.7-linux-x64/bin:$ATLAS_PROD/sw/atlasinstall_prod/R_install/bin/:$PATH
 
 cp ${ATLAS_PROD}/sw/atlasinstall_prod/atlasprod/analysis/gsa/api/gsaRestApiServer.js $ATLAS_PROD/atlas_gsa_api
 # gsaRestApiServer.js needs to be run from the same directory in which the modules it requires are installed, c.f. /nfs/production3/ma/home/rpetry/workspace/nodejs/node_modules
