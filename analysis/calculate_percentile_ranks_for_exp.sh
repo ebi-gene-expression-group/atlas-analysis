61d94e0ea8de8743a15a6c44f9515ed82cff8dca
wbazant <wbazant@ebi.ac.uk>
2017-06-26 18:25:02 +0100

    x

---
+++ analysis/calculate_percentile_ranks_for_exp.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# A wrapper script - so that this can be called from decorate_all_experiments.sh
+expAcc=$1
+expTargetDir=$2
+
+if [ $# -lt 2 ]; then
+  echo "Usage: $0 expAcc expPath"
+  exit 1
+fi
+
+pushd $expTargetDir
+
+$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/calculate_percentile_ranks_for_exp.pl
+
+popd
