274e1024501be0c0a80fde434c1989274c33a846
wbazant <wbazant@ebi.ac.uk>
2017-07-14 11:31:18 +0100

    A common interface to the experiment summary task. Do not log when the script has nothing interesting to say

--- analysis/atlasExperimentInR/createAtlasExperimentSummary.R
+++ analysis/atlasExperimentInR/createAtlasExperimentSummary.R
@@ -6,51 +6,29 @@
 # ExpressionSet (1-colour microarray data), or MAList (2-colour microarray
 # data).
 
-# Load ExpressionAtlasInternal package
 suppressMessages( library( ExpressionAtlasInternal ) )
+suppressMessages( library(optparse) )
 
-# Get commandline arguments.
-args <- commandArgs( TRUE )
-
-# Stop if we don't have exactly two arguments.
-if( length( args ) == 0 || length( args ) > 2 ) {
-	# Print a usage message.
-	stop( "\nUsage:\n\tcreateAtlasExperimentSummary.R <experiment accession> [ <Atlas experiment directory> ]\n\n" )
-
-# If there's one argument, this should be the experiment accession.
-} else if( length( args ) == 1 ) {
-	
-	# Get the accession from the first argument.
-	experimentAccession <- args[ 1 ]
-
-	# Use the current working directory as the experiment directory.
-	atlasExperimentDirectory <- getwd()
-
-# If there are two args, the first one should be the experiment accession and
-# the second one the atlas experiment directory.
-} else if( length( args ) == 2 ) {
-	
-	# Get the accession from the first argument.
-	experimentAccession <- args[ 1 ]
-
-	# Get the experiment directory from the second argument.
-	atlasExperimentDirectory <- args[ 2 ]
-}
-
-# Log where we're reading and writing.
-cat( paste("\nReading and writing files in", atlasExperimentDirectory, "\n\n" ) )
+args <- parse_args(OptionParser(option_list= list(
+	make_option(
+		c("-s", "--source"),
+		help="Source folder"
+	),
+	make_option(
+		c("-a", "--accession"),
+		help="Experiment accession"
+	),
+	make_option(
+	 c("-o", "--output"),
+	 help="Output file destination"
+	)
+)))
 
 # Create experiment summary SimpleList.
-experimentSummary <- summarizeAtlasExperiment( 
-	experimentAccession,
-	atlasExperimentDirectory
+experimentSummary <- summarizeAtlasExperiment(
+	args$accession,
+	args$source
 )
 
-# Create file name to save to.
-experimentSummaryFile <- paste( experimentAccession, "-atlasExperimentSummary.Rdata", sep="" )
-experimentSummaryFile <- file.path( atlasExperimentDirectory, experimentSummaryFile )
-
 # Save the object to a file.
-save( experimentSummary, file = experimentSummaryFile )
-
-cat( paste( "Experiment summary written to", experimentSummaryFile, "\n\n" ) )
+save( experimentSummary, file = args$output )
---
+++ analysis/atlasExperimentInR/create_atlas_experiment_summary.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# A wrapper script for the experiment summary
+# "strict mode"
+set -euo pipefail
+
+if [ $# -lt 1 ]; then
+        echo "Usage: $0 PATH_TO_EXPERIMENT "
+        echo "e.g. $0 ${ATLAS_PROD}/analysis/baseline/rna-seq/experiments/E-MTAB-513"
+        exit 1;
+fi
+
+expPath=$1
+e=`basename ${expPath}`
+output="$expPath/$e-atlasExperimentSummary.Rdata"
+
+$(dirname "${BASH_SOURCE[0]}")/createAtlasExperimentSummary.R \
+	--source "$expPath"\
+	--accession "$e" \
+	--output "$output"
+
+test -s "$output" || (  >&2 echo "$0 Failed to create summary: $expPath" ; exit 1 )
