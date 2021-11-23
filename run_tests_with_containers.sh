d1dccff3df7928f0a24fcd71c8d4ba67df26cec9
Jonathan Manning <jmanning@ebi.ac.uk>
2021-11-23 10:55:27 +0000

    Attept paths fix

--- run_tests_with_containers.sh
+++ run_tests_with_containers.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 docker run \
-  -v $(pwd)/tests:/usr/local/tests \
+  -v $(pwd):/usr/local/ \
   --entrypoint=/usr/local/tests/run-tests.bats \
   quay.io/ebigxa/atlas-analysis-base:0.0.1
