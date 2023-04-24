939d68f3c9bb9ad702d491e2722165ab117c20c5
Nadja Nolte <103439290+nadjano@users.noreply.github.com>
2023-04-24 15:40:38 +0100

    add correct script paths

--- deconvolution/run_deconvolution.sh
+++ deconvolution/run_deconvolution.sh
@@ -11,20 +11,20 @@ sc_reference_phenData=$5
 workflow_basedir=$6
 
 # Run FARDEEP
-Rscript ${workflow_basedir}/scripts/FARDEEP_run.R \
+Rscript ${workflow_basedir}/atlas-analysis/deconvolution/FARDEEP_run.R \
   Tissue_splits/${accession}/${accession}-${tissue}-fpkms_scaled.rds $sc_reference_C1 \
   Output/${accession}-${tissue}_res_FARDEEP.rds &
 mkdir -p scratch/${accession}/${accession}-${tissue}_scratch &
   
 # Run DWL (that takes some time...)
-Rscript ${workflow_basedir}/scripts/DWLS_run.R \
+Rscript ${workflow_basedir}/atlas-analysis/deconvolution/DWLS_run.R \
   Tissue_splits/${accession}/${accession}-${tissue}-fpkms_scaled.rds \
   $sc_reference_C0 $sc_reference_phenData 32 \
   Output/${accession}-${tissue}_res_DWLS.rds \
   scratch/${accession}/${accession}-${tissue}_scratch &
   
 # Run EpiDISH
-Rscript ${workflow_basedir}/scripts/EpiDISH_run.R \
+Rscript ${workflow_basedir}/atlas-analysis/deconvolution/EpiDISH_run.R \
   Tissue_splits/${accession}/${accession}-${tissue}-fpkms_scaled.rds \
   $sc_reference_C1 \
   Output/${accession}-${tissue}_res_EpiDISH.rds
