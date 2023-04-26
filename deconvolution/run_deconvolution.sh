9b226a0c7d028fd519aa05e2670ff33dacd13afc
Nadja Nolte <103439290+nadjano@users.noreply.github.com>
2023-04-26 11:19:59 +0100

    Update run_deconvolution.sh

--- deconvolution/run_deconvolution.sh
+++ deconvolution/run_deconvolution.sh
@@ -2,6 +2,7 @@
 
 # exit when any command fails
 set -e 
+
 # Assign input parameters to variables
 tissue=$1
 accession=$2
@@ -11,22 +12,35 @@ sc_reference_phenData=$5
 workflow_basedir=$6
 
 # Run FARDEEP
-Rscript ${workflow_basedir}/atlas-analysis/deconvolution/FARDEEP_run.R \
+fardeep_output=$(Rscript ${workflow_basedir}/atlas-analysis/deconvolution/FARDEEP_run.R \
   Tissue_splits/${accession}/${accession}-${tissue}-fpkms_scaled.rds $sc_reference_C1 \
-  Output/${accession}-${tissue}_res_FARDEEP.rds &
+  Output/${accession}-${tissue}_res_FARDEEP.rds 2>&1) || { 
+    echo "FARDEEP execution failed with error:" >&2 
+    echo "$fardeep_output" >&2 
+    exit 1 
+}
+
 mkdir -p scratch/${accession}/${accession}-${tissue}_scratch &
-  
+
 # Run DWL (that takes some time...)
-Rscript ${workflow_basedir}/atlas-analysis/deconvolution/DWLS_run.R \
+dwl_output=$(Rscript ${workflow_basedir}/atlas-analysis/deconvolution/DWLS_run.R \
   Tissue_splits/${accession}/${accession}-${tissue}-fpkms_scaled.rds \
   $sc_reference_C0 $sc_reference_phenData 32 \
   Output/${accession}-${tissue}_res_DWLS.rds \
-  scratch/${accession}/${accession}-${tissue}_scratch &
-  
+  scratch/${accession}/${accession}-${tissue}_scratch 2>&1) || { 
+    echo "DWL execution failed with error:" >&2 
+    echo "$dwl_output" >&2 
+    exit 1 
+}
+
 # Run EpiDISH
-Rscript ${workflow_basedir}/atlas-analysis/deconvolution/EpiDISH_run.R \
+epidish_output=$(Rscript ${workflow_basedir}/atlas-analysis/deconvolution/EpiDISH_run.R \
   Tissue_splits/${accession}/${accession}-${tissue}-fpkms_scaled.rds \
   $sc_reference_C1 \
-  Output/${accession}-${tissue}_res_EpiDISH.rds
+  Output/${accession}-${tissue}_res_EpiDISH.rds 2>&1) || { 
+    echo "EpiDISH execution failed with error:" >&2 
+    echo "$epidish_output" >&2 
+    exit 1 
+}
 
 wait
