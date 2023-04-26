468870ec360c1d9bde24fcd20ea2612f93a897d0
Nadja Nolte <103439290+nadjano@users.noreply.github.com>
2023-04-26 10:45:06 +0100

    Update splitAndScale.R

--- deconvolution/splitAndScale.R
+++ deconvolution/splitAndScale.R
@@ -12,16 +12,17 @@ suppressMessages(library(dplyr))
 split_counts_per_tissue <- function(bulk_counts, design_file){
     bulk_counts_splits = list()
     # get all tissues from sdrf file that are present in experiment
-    organism_parts = as.character(unique(design_file$Characteristics.organism.part.))
+    organism_parts <- as.character(unique(design_file$Characteristics.organism.part.))
   
-    if (length(organism_parts)== 0){   
+    if (length(organism_parts) == 0){   
         organism_parts <- as.character(unique(design_file$Characteristics..organism.part.))
         design_file$Characteristics.organism.part. <- design_file$Characteristics..organism.part.
     }
     #do a check on the sdrf file   
-    if (length(organism_parts)== 0){   
-        stop("sdrf file does not include collum Characteristics[organism.part] or Characteristics [organism.part]. Make sure organism part names are
-            stored in this collumn")
+    if (length(organism_parts) == 0){   
+        stop(paste("sdrf file does not include collum Characteristics[organism.part] or",
+                   "Characteristics [organism.part]. Make sure organism part names are",
+                   "stored in this collumn"))
     }
 
     tissue_dict = c()
@@ -32,7 +33,7 @@ split_counts_per_tissue <- function(bulk_counts, design_file){
    
         # extract column from count file for each tissue
         bulk_counts_split = bulk_counts[,colnames(bulk_counts) %in% tissue_runs]
-        bulk_counts_splits <- append(bulk_counts_splits, list(bulk_counts_split))
+        bulk_counts_splits = append(bulk_counts_splits, list(bulk_counts_split))
     
     }
     names(bulk_counts_splits) = organism_parts
@@ -40,63 +41,56 @@ split_counts_per_tissue <- function(bulk_counts, design_file){
 }
 
 args <- commandArgs(trailingOnly = TRUE)
-fpkms = args[1]
-sdrf_filename = args[2]
-exp_name = args[3]
+fpkms <- args[1]
+sdrf_filename <- args[2]
+exp_name <- args[3]
 
 
 if(length(args) != 3) {
    stop("Not correct number of arguments. Please supply three arguments")
 }
 
-fpkms = read.csv(fpkms, sep = "\t", check.names = FALSE, row.names = 1)
+fpkms <- read.csv(fpkms, sep = "\t", check.names = FALSE, row.names = 1)
 
-# remove duplicated columns in fpkms 
-fpkms = fpkms %>% 
+# remove columns with duplicated columnnames in fpkms 
+fpkms <- fpkms %>% 
   select(unique(colnames(.)))
 
 sdrf = read.csv(sdrf_filename, sep = "\t", row.names = NULL)
 
 # Rename row names to values in "Comment.ENA_RUN." or "Comment..ENA_RUN." column
 if ("Comment.ENA_RUN." %in% colnames(sdrf)) {
-  sdrf = sdrf[!duplicated(sdrf[ , "Comment.ENA_RUN."]),]
+  sdrf <- sdrf[!duplicated(sdrf[ , "Comment.ENA_RUN."]),]
   rownames(sdrf) <- sdrf$Comment.ENA_RUN.
 } else if ("Comment..ENA_RUN." %in% colnames(sdrf)) {
-  sdrf = sdrf[!duplicated(sdrf[ , "Comment..ENA_RUN."]),]
+  sdrf <- sdrf[!duplicated(sdrf[ , "Comment..ENA_RUN."]),]
   rownames(sdrf) <- sdrf$Comment..ENA_RUN.
 } else {
   stop("Neither 'Comment.ENA_RUN.' nor 'Comment..ENA_RUN.' column found in file.")
 }
 
-
-
 # split fpkms into runs per organism part
-tissue_splits = split_counts_per_tissue(fpkms, sdrf)
+tissue_splits <- split_counts_per_tissue(fpkms, sdrf)
 
 # set methods to scale sdrf counts
 method <- 'TMM'
 plan('multisession', workers = as.numeric('32')) #Paralellism
 for (i in seq_along(tissue_splits)){
-
-    # T <- readRDS(split)
-    #filename_T <- sub("Tissue_splits/", "Tissue_splits/Normalized_tables/", filename_T)
-
-    #Preprocess matrices to avoid errorshaving multiple snakefiles in one directory
-    tissue = names(tissue_splits)[i]
-    tissue_split = tissue_splits[[tissue]]
-    print(head(tissue_split))
+    tissue <- names(tissue_splits)[i]
+    tissue_split <- tissue_splits[[tissue]]
+  
     message(paste0("Scaling T with method: ", method))
 
-    #Only get rows where there is at least one cell with a count
+    # Only get rows where there is at least one cell with a count
     tissue_split <- tissue_split[rowSums(tissue_split) != 0,]
     print(head(tissue_split))
-    #Same for cols
+    # Same for cols
     tissue_split <- tissue_split[,colSums(tissue_split) != 0]
     print(head(tissue_split))
-    #Only keep rows with different counts after transformations
+    # Only keep rows with different counts after transformations
     tissue_split <- tissue_split[!apply(tissue_split, 1, function(x) var(x) == 0),]
 
-    #Switch modes
+    # Switch modes
     switch(method,
     
     "TMM" = {
@@ -107,13 +101,13 @@ for (i in seq_along(tissue_splits)){
         tissue_split <- edgeR::cpm(tissue_split)
     }
     )
-    #Only get rows where there is at least one cell with a count
+    # Only get rows where there is at least one cell with a count
     tissue_split <- tissue_split[rowSums(tissue_split) != 0,]
 
-    #Same for cols
+    # Same for cols
     tissue_split <- tissue_split[,colSums(tissue_split) != 0]
 
-    #Only keep rows with different counts after transformations
+    # Only keep rows with different counts after transformations
     tissue_split <- tissue_split[!apply(tissue_split, 1, function(x) var(x) == 0),]
 
     # Save transformed tables
