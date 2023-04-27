fae3e741258f10ed971902b3a19c5d2812b21290
Nadja Nolte <103439290+nadjano@users.noreply.github.com>
2023-04-27 11:47:01 +0100

    change output location

--- deconvolution/run_deconvolution.sh
+++ deconvolution/run_deconvolution.sh
@@ -15,7 +15,7 @@ export DECONV_STATUS="deconvolution successful"
 echo "start running FARDEEP"
 fardeep_output=$(Rscript ${workflow_basedir}/atlas-analysis/deconvolution/FARDEEP_run.R \
   Tissue_splits/${accession}/${accession}-${tissue}-fpkms_scaled.rds $sc_reference_C1 \
-  Output/${accession}-${tissue}_res_FARDEEP.rds 2>&1) || { 
+  Output/${accession}/${accession}-${tissue}_res_FARDEEP.rds 2>&1) || { 
     echo "FARDEEP execution failed with error:" >&2 
     echo "$fardeep_output" >&2 
     export DECONV_STATUS="FARDEEP failed"
@@ -28,7 +28,7 @@ echo "start running DWLS, that takes some time.."
 dwl_output=$(Rscript ${workflow_basedir}/atlas-analysis/deconvolution/DWLS_run.R \
   Tissue_splits/${accession}/${accession}-${tissue}-fpkms_scaled.rds \
   $sc_reference_C0 $sc_reference_phenData 32 \
-  Output/${accession}-${tissue}_res_DWLS.rds \
+  Output/${accession}/${accession}-${tissue}_res_DWLS.rds \
   scratch/${accession}/${accession}-${tissue}_scratch 2>&1) || { 
     echo "DWLS execution failed with error:" >&2 
     echo "$dwl_output" >&2 
@@ -40,7 +40,7 @@ echo "started running EpiDISH"
 epidish_output=$(Rscript ${workflow_basedir}/atlas-analysis/deconvolution/EpiDISH_run.R \
   Tissue_splits/${accession}/${accession}-${tissue}-fpkms_scaled.rds \
   $sc_reference_C1 \
-  Output/${accession}-${tissue}_res_EpiDISH.rds 2>&1) || { 
+  Output/${accession}/${accession}-${tissue}_res_EpiDISH.rds 2>&1) || { 
     echo "EpiDISH execution failed with error:" >&2 
     echo "$epidish_output" >&2 
     export DECONV_STATUS="EpiDISH failed"
