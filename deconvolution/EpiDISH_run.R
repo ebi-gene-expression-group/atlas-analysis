7066d7084cb81856eacbb6f62f1addf2fb648101
Nadja Nolte <103439290+nadjano@users.noreply.github.com>
2023-06-30 10:24:09 +0100

    Update EpiDISH_run.R

--- deconvolution/EpiDISH_run.R
+++ deconvolution/EpiDISH_run.R
@@ -6,7 +6,7 @@
 
 suppressMessages(library('EpiDISH'))
 
-# Get args and load files
+# get args and load files
 args <- commandArgs(trailingOnly = TRUE)
 
 if (length(args) != 3) {
@@ -20,17 +20,20 @@ filename_output <- args[3]
 fpkms <- readRDS(filename_fpkms)
 sc_reference <- readRDS(filename_sc_reference)
 
-
-# Toss out the genes tossed out in T normalization from C as well
+# get intersection of genes
 common <- intersect(rownames(sc_reference), rownames(fpkms))
+# check if common contains values
+if (length(common) == 0) {
+  stop("No common genes between sc reference and fpkms found. Check that both have ENSEMBLE ids as rownames")
+}
 sc_reference <- sc_reference[common,]
 fpkms  <- fpkms[common,]
 sc_reference <- as.matrix(sc_reference) #Explicit typecasting needed
 
-# Get res and reorder the matrices for correspondence
+# get res and reorder the matrices for correspondence
 results <- t(EpiDISH::epidish(beta.m = fpkms, ref.m = sc_reference, method = "RPC")$estF)
 results <- apply(results,2,function(x) ifelse(x < 0, 0, x)) #explicit non-negativity constraint
 results <- apply(results, 2,function(x) x/sum(x)) #explicit STO constraint
 
-# Save and exit
+# save and exit
 saveRDS(results, file=filename_output)
