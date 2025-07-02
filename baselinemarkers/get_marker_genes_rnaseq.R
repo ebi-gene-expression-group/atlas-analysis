d2a627536f72ea733329c22e8c3dfee446387d9f
Pedro Madrigal <8195212+pmb59@users.noreply.github.com>
2025-07-02 16:30:05 +0100

    remove 'assay_id' and 'number_assays' columns, for now

--- baselinemarkers/get_marker_genes_rnaseq.R
+++ baselinemarkers/get_marker_genes_rnaseq.R
@@ -365,6 +365,9 @@ filtered_df_sorted <- filtered_df_sorted %>%
 
 colnames(filtered_df_sorted) <- c("experiment_accession", "assay_id", "assay", "gene_id", "specificity_score", "marker_gene_rank", "expression_unit", "number_assays", "expression_level", "gene_name")
 
+# remove 'assay_id' and 'number_assays' columns, for now
+filtered_df_sorted <- filtered_df_sorted[, !(names(filtered_df_sorted) %in% c("assay_id", "number_assays"))]
+
 print ( head(filtered_df_sorted) )
 
 ####################################
